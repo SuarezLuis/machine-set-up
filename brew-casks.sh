222002d59ae2ee57b56e431290919ab2d6e57a7a
Luis Suarez <suarezluis@gmail.com>
2024-03-03 19:06:06 -0600

    Add some scripts

---
+++ brew-casks.sh
@@ -0,0 +1,41 @@
+
+# Install Oh My Zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install Brew Casks
+brew install iterm2 # Terminal
+brew install raycast # replacement for spotlight
+brew install rectangle # window manager
+brew install alt-tab # window manager
+brew install android-file-transfer # Android File Transfer
+brew install android-platform-tools # Android Platform Tools
+brew install keepingyouawake # Caffeine
+brew install discord # Discord Chat
+brew install slack # Slack Chat
+brew install vlc # VLC Media Player
+brew install keka # Keka File Archiver
+brew install kap # Kap Screen Recorder
+brew install time-out # Time Out Break Reminder
+brew install figma # Figma Design Tool
+brew install visual-studio-code # Visual Studio Code
+brew install sublime-text # Sublime Text Editor
+
+# Install Brew Formulae
+brew install ffmpeg # Video Converter
+brew install imagemagick # Image Converter
+brew install wget # Download Manager
+brew install telnet # Telnet Client
+brew install tldr # Alternative to man pages
+
+# Install NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+# Install NPM global packages
+npm install -g srt-translate # SRT Subtitle Translator
+npm install -g open-cu # Open ClickUp
+npm install -g kill-port # Kill Port
+npm install -g gitopen # Open Github Repository
+npm install -g git-standup # Git Standup
